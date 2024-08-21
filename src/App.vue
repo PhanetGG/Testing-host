fe66810a9bc7a5af7013c50779230640a6e6345f
heangphanet <phanet.heang@sunfixconsulting.com>
2024-08-21 11:54:32 +0700

    fist

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+      <nav>
+        <!-- <router-link to="/">Home</router-link> -->
+      </nav>
+      <router-view/> <!-- This is where routed components will be displayed -->
+    </div>
+  </template>
+  <script setup>
+  </script>
+  
+  <style>
+  /* Add styles here */
+  </style>
+  
